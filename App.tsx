158ee6bc2446887f9fbabfe7d5d3d91ccf5bfa6b
Rodriasd <veronrodrigo98@gmail.com>
2024-05-14 12:09:39 -0300

    added the page 'GameInfo'

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { Games } from "./pages/Games";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { GameInfo } from "./pages/GameInfo";
 
 const Stack = createNativeStackNavigator()
 
@@ -13,6 +14,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen  name="GameList" component={Games} options={{headerShown:false}}/>
+        <Stack.Screen name="GameInfo" component={GameInfo}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
